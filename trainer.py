fa73b97c003165dc1ef0f835b47b041aa1fbef20
Yuzhi ZHAO <yzzhao2-c@my.cityu.edu.hk>
2021-12-12 12:55:28 +0800

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -15,7 +15,148 @@ from scipy.io import loadmat
 import dataset
 import utils
 
-def L1_Trainer(opt):
+def Trainer(opt):
+    # ----------------------------------------
+    #       Network training parameters
+    # ----------------------------------------
+
+    # cudnn benchmark
+    cudnn.benchmark = opt.cudnn_benchmark
+
+    # configurations
+    tail_name = opt.network
+    save_folder = os.path.join(opt.save_path, tail_name)
+    sample_folder = os.path.join(opt.sample_path, tail_name)
+    utils.check_path(save_folder)
+    utils.check_path(sample_folder)
+
+    # Loss functions
+    criterion_L1 = torch.nn.L1Loss().cuda()
+
+    # Initialize Generator
+    generator = utils.create_generator(opt)
+
+    # To device
+    if opt.multi_gpu:
+        generator = nn.DataParallel(generator)
+        generator = generator.cuda()
+    else:
+        generator = generator.cuda()
+
+    # Optimizers
+    optimizer_G = torch.optim.Adam(generator.parameters(), lr = opt.lr_g, betas = (opt.b1, opt.b2), weight_decay = opt.weight_decay)
+    
+    # Learning rate decrease
+    def adjust_learning_rate(opt, epoch, optimizer):
+        target_epoch = opt.epochs - opt.lr_decrease_epoch
+        remain_epoch = opt.epochs - epoch
+        if epoch >= opt.lr_decrease_epoch:
+            lr = opt.lr_g * remain_epoch / target_epoch
+            for param_group in optimizer.param_groups:
+                param_group['lr'] = lr
+    
+    # Save the model if pre_train == True
+    def save_model(opt, epoch, iteration, len_dataset, generator):
+        """Save the model at "checkpoint_interval" and its multiple"""
+        # Define the name of trained model
+        if opt.save_mode == 'epoch':
+            model_name = '%s_epoch%d_bs%d.pth' % (opt.network, epoch, opt.train_batch_size)
+        if opt.save_mode == 'iter':
+            model_name = '%s_iter%d_bs%d.pth' % (opt.network, iteration, opt.train_batch_size)
+        save_model_path = os.path.join(save_folder, model_name)
+        if opt.multi_gpu == True:
+            if opt.save_mode == 'epoch':
+                if (epoch % opt.save_by_epoch == 0) and (iteration % len_dataset == 0):
+                    torch.save(generator.module.state_dict(), save_model_path)
+                    print('The trained model is successfully saved at epoch %d' % (epoch))
+            if opt.save_mode == 'iter':
+                if iteration % opt.save_by_iter == 0:
+                    torch.save(generator.module.state_dict(), save_model_path)
+                    print('The trained model is successfully saved at iteration %d' % (iteration))
+        else:
+            if opt.save_mode == 'epoch':
+                if (epoch % opt.save_by_epoch == 0) and (iteration % len_dataset == 0):
+                    torch.save(generator.state_dict(), save_model_path)
+                    print('The trained model is successfully saved at epoch %d' % (epoch))
+            if opt.save_mode == 'iter':
+                if iteration % opt.save_by_iter == 0:
+                    torch.save(generator.state_dict(), save_model_path)
+                    print('The trained model is successfully saved at iteration %d' % (iteration))
+
+    # ----------------------------------------
+    #             Network dataset
+    # ----------------------------------------
+
+    # Handle multiple GPUs
+    gpu_num = torch.cuda.device_count()
+    print("There are %d GPUs used" % gpu_num)
+    opt.train_batch_size *= gpu_num
+    opt.num_workers *= gpu_num
+
+    # Define the dataset
+    trainset = dataset.SIDD_Dataset(opt)
+    print('The overall number of training images:', len(trainset))
+
+    # Define the dataloader
+    train_loader = DataLoader(trainset, batch_size = opt.train_batch_size, shuffle = True, num_workers = opt.num_workers, pin_memory = True)
+    
+    # ----------------------------------------
+    #                 Training
+    # ----------------------------------------
+
+    # Count start time
+    prev_time = time.time()
+    
+    # For loop training
+    for epoch in range(opt.epochs):
+        for i, (input_batch, gt_batch) in enumerate(train_loader):
+
+            # To device
+            input_batch = input_batch.cuda()
+            gt_batch = gt_batch.cuda()
+
+            # Process patch
+            if len(input_batch.shape) == 5:
+                _, _, C, H, W = input_batch.shape # B, B, C, H, W
+                input_batch = input_batch.view(-1, C, H, W)
+                gt_batch = gt_batch.view(-1, C, H, W)
+
+            # Train Generator
+            optimizer_G.zero_grad()
+            gen_batch, _ = generator(input_batch)
+            
+            # L1 Loss
+            L1_Loss = criterion_L1(gen_batch, gt_batch)
+
+            # Overall Loss and optimize
+            loss = L1_Loss
+            loss.backward()
+            optimizer_G.step()
+
+            # Determine approximate time left
+            iters_done = epoch * len(train_loader) + i
+            iters_left = opt.epochs * len(train_loader) - iters_done
+            time_left = datetime.timedelta(seconds = iters_left * (time.time() - prev_time))
+            prev_time = time.time()
+
+            # Print log
+            print("\r[Epoch %d/%d] [Batch %d/%d] [L1 Loss: %.4f] Time_left: %s" %
+                ((epoch + 1), opt.epochs, i, len(train_loader), L1_Loss.item(), time_left))
+
+            # Save model at certain epochs or iterations
+            save_model(opt, (epoch + 1), (iters_done + 1), len(train_loader), generator)
+
+            # Learning rate decrease at certain epochs
+            adjust_learning_rate(opt, (epoch + 1), optimizer_G)
+            
+        ### Sample data every epoch
+        if (epoch + 1) % 1 == 0:
+            img_list = [input_batch, gen_batch, gt_batch]
+            name_list = ['input', 'pred', 'gt']
+            utils.save_sample_png(sample_folder = sample_folder, sample_name = 'train_epoch%d' % (epoch + 1), \
+                img_list = img_list, name_list = name_list, pixel_max_cnt = 255)
+
+def Trainer_Multilevel(opt):
     # ----------------------------------------
     #       Network training parameters
     # ----------------------------------------
@@ -164,147 +305,6 @@ def L1_Trainer(opt):
             # Print log
             print("\r[Epoch %d/%d] [Batch %d/%d] [Loss: %.4f] [L1/TanhL1 Loss: %.4f] Time_left: %s" %
                 ((epoch + 1), opt.epochs, i, len(train_loader), loss.item(), L1_Loss.item(), time_left))
-
-            # Save model at certain epochs or iterations
-            save_model(opt, (epoch + 1), (iters_done + 1), len(train_loader), generator)
-
-            # Learning rate decrease at certain epochs
-            adjust_learning_rate(opt, (epoch + 1), optimizer_G)
-            
-        ### Sample data every epoch
-        if (epoch + 1) % 1 == 0:
-            img_list = [input_batch, gen_batch, gt_batch]
-            name_list = ['input', 'pred', 'gt']
-            utils.save_sample_png(sample_folder = sample_folder, sample_name = 'train_epoch%d' % (epoch + 1), \
-                img_list = img_list, name_list = name_list, pixel_max_cnt = 255)
-
-def TanhL1_Trainer(opt):
-    # ----------------------------------------
-    #       Network training parameters
-    # ----------------------------------------
-
-    # cudnn benchmark
-    cudnn.benchmark = opt.cudnn_benchmark
-
-    # configurations
-    tail_name = opt.network
-    save_folder = os.path.join(opt.save_path, tail_name)
-    sample_folder = os.path.join(opt.sample_path, tail_name)
-    utils.check_path(save_folder)
-    utils.check_path(sample_folder)
-
-    # Loss functions
-    criterion_L1 = torch.nn.L1Loss().cuda()
-
-    # Initialize Generator
-    generator = utils.create_generator(opt)
-
-    # To device
-    if opt.multi_gpu:
-        generator = nn.DataParallel(generator)
-        generator = generator.cuda()
-    else:
-        generator = generator.cuda()
-
-    # Optimizers
-    optimizer_G = torch.optim.Adam(generator.parameters(), lr = opt.lr_g, betas = (opt.b1, opt.b2), weight_decay = opt.weight_decay)
-    
-    # Learning rate decrease
-    def adjust_learning_rate(opt, epoch, optimizer):
-        target_epoch = opt.epochs - opt.lr_decrease_epoch
-        remain_epoch = opt.epochs - epoch
-        if epoch >= opt.lr_decrease_epoch:
-            lr = opt.lr_g * remain_epoch / target_epoch
-            for param_group in optimizer.param_groups:
-                param_group['lr'] = lr
-    
-    # Save the model if pre_train == True
-    def save_model(opt, epoch, iteration, len_dataset, generator):
-        """Save the model at "checkpoint_interval" and its multiple"""
-        # Define the name of trained model
-        if opt.save_mode == 'epoch':
-            model_name = '%s_epoch%d_bs%d.pth' % (opt.network, epoch, opt.train_batch_size)
-        if opt.save_mode == 'iter':
-            model_name = '%s_iter%d_bs%d.pth' % (opt.network, iteration, opt.train_batch_size)
-        save_model_path = os.path.join(save_folder, model_name)
-        if opt.multi_gpu == True:
-            if opt.save_mode == 'epoch':
-                if (epoch % opt.save_by_epoch == 0) and (iteration % len_dataset == 0):
-                    torch.save(generator.module.state_dict(), save_model_path)
-                    print('The trained model is successfully saved at epoch %d' % (epoch))
-            if opt.save_mode == 'iter':
-                if iteration % opt.save_by_iter == 0:
-                    torch.save(generator.module.state_dict(), save_model_path)
-                    print('The trained model is successfully saved at iteration %d' % (iteration))
-        else:
-            if opt.save_mode == 'epoch':
-                if (epoch % opt.save_by_epoch == 0) and (iteration % len_dataset == 0):
-                    torch.save(generator.state_dict(), save_model_path)
-                    print('The trained model is successfully saved at epoch %d' % (epoch))
-            if opt.save_mode == 'iter':
-                if iteration % opt.save_by_iter == 0:
-                    torch.save(generator.state_dict(), save_model_path)
-                    print('The trained model is successfully saved at iteration %d' % (iteration))
-
-    # ----------------------------------------
-    #             Network dataset
-    # ----------------------------------------
-
-    # Handle multiple GPUs
-    gpu_num = torch.cuda.device_count()
-    print("There are %d GPUs used" % gpu_num)
-    opt.train_batch_size *= gpu_num
-    opt.num_workers *= gpu_num
-
-    # Define the dataset
-    trainset = dataset.SIDD_Dataset(opt)
-    print('The overall number of training images:', len(trainset))
-
-    # Define the dataloader
-    train_loader = DataLoader(trainset, batch_size = opt.train_batch_size, shuffle = True, num_workers = opt.num_workers, pin_memory = True)
-    
-    # ----------------------------------------
-    #                 Training
-    # ----------------------------------------
-
-    # Count start time
-    prev_time = time.time()
-    
-    # For loop training
-    for epoch in range(opt.epochs):
-        for i, (input_batch, gt_batch) in enumerate(train_loader):
-
-            # To device
-            input_batch = input_batch.cuda()
-            gt_batch = gt_batch.cuda()
-
-            # Process patch
-            if len(input_batch.shape) == 5:
-                _, _, C, H, W = input_batch.shape # B, B, C, H, W
-                input_batch = input_batch.view(-1, C, H, W)
-                gt_batch = gt_batch.view(-1, C, H, W)
-
-            # Train Generator
-            optimizer_G.zero_grad()
-            gen_batch, _ = generator(input_batch)
-            
-            # L1 Loss
-            L1_Loss = criterion_L1(gen_batch, gt_batch)
-
-            # Overall Loss and optimize
-            loss = L1_Loss
-            loss.backward()
-            optimizer_G.step()
-
-            # Determine approximate time left
-            iters_done = epoch * len(train_loader) + i
-            iters_left = opt.epochs * len(train_loader) - iters_done
-            time_left = datetime.timedelta(seconds = iters_left * (time.time() - prev_time))
-            prev_time = time.time()
-
-            # Print log
-            print("\r[Epoch %d/%d] [Batch %d/%d] [L1 Loss: %.4f] Time_left: %s" %
-                ((epoch + 1), opt.epochs, i, len(train_loader), L1_Loss.item(), time_left))
 
             # Save model at certain epochs or iterations
             save_model(opt, (epoch + 1), (iters_done + 1), len(train_loader), generator)
